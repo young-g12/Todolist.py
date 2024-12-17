746b60f1a0cc2d353454cc8de9bf48c901a370f8
young-g12 <74071824+young-g12@users.noreply.github.com>
2024-12-17 06:58:12 -0500

    removed repetitive code

--- index.py
+++ index.py
@@ -2,20 +2,20 @@
 task_list = []
 
 while True:
-    display_options = ["add task", "remove task", "view task", "exit"]
+    display_options.lower() = ["add task", "remove task", "view task", "exit"]
     print(display_options)
     choose_task = input("Which choice would you like to make?: ")
-    if choose_task == display_options[0].lower():
+    if choose_task == display_options[0]:
         print("add task")
         input_task = input("enter task: ")
         task_list.append(input_task)
         print(input_task)
-    elif choose_task == display_options[1].lower():
+    elif choose_task == display_options[1]:
         task_list.remove(input(f"Which task would you like to remove?: "))
         print("Task removed")
-    elif choose_task == display_options[2].lower():
+    elif choose_task == display_options[2]:
         print(task_list)
-    elif choose_task == display_options[3].lower():
+    elif choose_task == display_options[3]:
         break
     else:
         print("Invalid option, please choose another choice")
